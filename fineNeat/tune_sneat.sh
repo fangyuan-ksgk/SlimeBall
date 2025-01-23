272cc61b3b654fb283e2edc7ac7b1b72b4a683e7
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-01-23 12:19:01 +0800

    twist on population selection for selfplay, as well as matching opponent

--- fineNeat/tune_sneat.sh
+++ fineNeat/tune_sneat.sh
@@ -1,23 +1,14 @@
-# Non-SelfPlay :: global scoring
+# Non-SelfPlay :: global scoring -- works 
 python sneat_tune.py \
     --logdir ../runs/sneat_tune_non_sp \
     --checkpoint ../zoo/sneat_check/sneat_00360000_small.json \
     --total-tournaments 480000 &
 
-# SelfPlay :: discounted winning streak 
+# SelfPlay :: discounted winning streak -- does not work -- it's worse than accumulated winning streak
 python sneat_tune.py \
     --selfplay \
     --logdir ../runs/sneat_tune_sp \
     --checkpoint ../zoo/sneat_check/sneat_00360000_small.json \
-    --mut-discount 1.0 \
-    --total-tournaments 480000 &
-
-# SelfPlay :: discounted winning streak :: with longer period
-python sneat_tune.py \
-    --selfplay \
-    --logdir ../runs/sneat_tune_sp_long \
-    --checkpoint ../zoo/sneat_check/sneat_00360000_small.json \
-    --period 5000 \
     --mut-discount 1.0 \
     --total-tournaments 480000 &
 
